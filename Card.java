b844c2f4035f5d51e3e9d39bc038bc12c1c5b392
florianzymeri <zymeri@cip.ifi.lmu.de>
2023-10-20 03:11:22 +0200

    Gamecode größtenteils gecoded

--- Card.java
+++ Card.java
@@ -1,2 +1,17 @@
 public class Card {
+    private String name;
+    private String effect;
+
+    public Card(String name, String effect) {
+        this.name = name;
+        this.effect = effect;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
 }
--- LoveLetterGame.java
+++ LoveLetterGame.java
@@ -14,57 +14,75 @@ public class LoveLetterGame {
     public void addPlayer(String name) {
         players.add(new Player(name));
     }
+
     public void startGame() {
         //Spiel wird gestartet
     }
+
     public void playCard(String card) {
         //Karte wird gespielt
     }
+
     public void showHand() {
         //Spielhand wird gezeigt
     }
+
     public void showScore() {
         //Zeigt den Score
     }
+
     public static void main(String[] args) {
-        LoveLetterGame game = new LoveLetterGame();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("******Willkomen zu Love Letter******");
+        try {
+            LoveLetterGame game = new LoveLetterGame();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("************************************************");
+            System.out.println("**********  Willkomen zu Love Letter  **********");
+            System.out.println("************************************************");
+            Thread.sleep(1500);
+            int numPlayers;
+            do {
+                System.out.println("Wie viele Spieler möchten Spielen");
+                System.out.println("(min: 2 max: 4)");
+                numPlayers = scanner.nextInt();
+                scanner.nextLine();
+            } while (numPlayers < 2 || numPlayers > 4);
 
-        int numPlayers;
-        do {
-            System.out.println("Wie viele Spieler möchten Spielen (min:2 , max:4)");
-            numPlayers = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
-        } while (numPlayers < 2 || numPlayers > 4);
-
-        for (int i = 0; i < numPlayers; i++) {
-            System.out.print("Name von Spieler Nr." + (i + 1)+":    ");
-            String playerName = scanner.nextLine();
-            game.addPlayer(playerName);
-        }
-
-        System.out.println("Spielbefehle: \\start (Startet das Spiel)");
-        System.out.println("              \\help (Zeigt dir alle möglichen Befehle)");
-
-        while (true) {
-            System.out.print("Enter a game command: ");
-            String command = scanner.nextLine();
-            if (command.equals("\\start")) {
-                game.startGame();
-            } else if (command.equals("\\playCard")) {
-                System.out.print("Enter the card you want to play: ");
-                String card = scanner.nextLine();
-                game.playCard(card);
-            } else if (command.equals("\\showHand")) {
-                game.showHand();
-            } else if (command.equals("\\showScore")) {
-                game.showScore();
-            } else if (command.equals("\\help")) {
-                System.out.println("Available commands: \\start, \\playCard, \\showHand, \\showScore, \\help");
-            } else {
-                System.out.println("Unzulässiger Befehl. \\help für weitere Möglichkeiten.");
+            for (int i = 0; i < numPlayers; i++) {
+                System.out.print("Name von Spieler Nr." + (i + 1) + ":    ");
+                String playerName = scanner.nextLine();
+                game.addPlayer(playerName);
             }
+            Thread.sleep(1500);
+            System.out.println("Spielbefehle: \\start (Startet das Spiel)");
+            System.out.println("\\start         Startet das Spiel");
+            System.out.println("\\help          Zeigt dir alle möglichen Befehle");
+
+            while (true) {
+                System.out.print("Gebe einen Spielbefehl ein: ");
+                String command = scanner.nextLine();
+                if (command.equalsIgnoreCase("\\start")) {
+                    game.startGame();
+                } else if (command.equals("\\playCard")) {
+                    System.out.print("Welche Karte willst du spielen ?: ");
+                    String card = scanner.nextLine();
+                    game.playCard(card);
+                } else if (command.equalsIgnoreCase("\\showHand")) {
+                    game.showHand();
+                } else if (command.equalsIgnoreCase("\\showScore")) {
+                    game.showScore();
+                } else if (command.equalsIgnoreCase("\\help")) {
+                    System.out.println("Befehle:");
+                    System.out.println("\\start         Spiel wird gestartet");
+                    System.out.println("\\playCard      Karte wird gespielt");
+                    System.out.println("\\showHand      Spielhand wird dir gezeigt");
+                    System.out.println("\\showScore     Zeigt dir den Score");
+                } else {
+                    System.out.println("Unzulässiger Befehl \\help für Befehlsliste");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Ein Fehler ist aufgetreten: " + e.getMessage());
         }
     }
+
 }
